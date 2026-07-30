6a35b381837b6905a25d8950b6985f71a6f2c8b8
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-07-30 14:31:33 +0100

    Learning TS continues

--- Basics/src/09-url-module.ts
+++ Basics/src/09-url-module.ts
@@ -1,12 +1,30 @@
 
-function url():void{
-    const url = new URL("https://www.acedev.com/users/page=2&limit=15&sort=lates")
-    console.log(url.hostname)
-    console.log(url.host)
-    console.log(url.pathname)
+function url(): void {
+    const url = new URL("https://www.acedev.com/users/?page=2&limit=15&sort=lates")
+    // console.log(url.host, url.hostname, url.href, url.protocol, url.search)
+
+    const page = url.searchParams.get("page")
+    const sort = url.searchParams.get("sort")
+    const limit = url.searchParams.get("limit")
+
+    console.log(page, sort, limit)
+
+    url.searchParams.set("page", "30")
+    url.searchParams.set("limit", "30")
+    url.searchParams.set("sort", "questions")
+
     console.log(url.href)
-    console.log(url.protocol)
-    console.log(url.search)
+
+    const queryParams = new URLSearchParams({
+        page: "40", 
+        limit: "40",
+        sort: "dealings"
+
+    })
+
+    console.log(queryParams)
+    console.log(queryParams.toString())
 }
+
 
 url()
