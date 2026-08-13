a38e93d16ae37b379359dfad63745bba55080eea
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-08-13 16:04:54 +0100

    LEARNING POSTGRES

--- postgres/part1/first-database.sql
+++ postgres/part1/first-database.sql
@@ -1,6 +1,5 @@
 -- If database exists, then drop, if not, it  will create
-DROP IF EXISTS DATABASE postgressql_part1
+DROP DATABASE IF EXISTS  postgressql_part1;
 
 -- Create the database in my postgre server
-CREATE DATABASE postgressql_part1 
-
+CREATE DATABASE postgressql_part1;
