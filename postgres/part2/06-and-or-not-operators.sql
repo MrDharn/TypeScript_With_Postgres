671ea782a7b7637c1b717788a3b16881a5246339
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-08-27 12:25:33 +0100

    Learning Postgres

--- postgres/part2/06-and-or-not-operators.sql
+++ postgres/part2/06-and-or-not-operators.sql
@@ -4,4 +4,4 @@ AND price = 249.99;
   
 SELECT name, price, sku FROM products
 WHERE sku IN ('TECH-WNC-00X1','TECH-WNC-00X2','TECH-WNC-00X3') 
-OR price = 249.99;
+OR NOT price = 249.99;
---
+++ postgres/part2/07-like-ilike-pattern.sql
@@ -0,0 +1,5 @@
+-- SELECT name, price FROM products
+-- WHERE name LIKE 'Stainless%';
+
+SELECT name , price FROM products
+WHERE name ILIKE 'wireless% '
