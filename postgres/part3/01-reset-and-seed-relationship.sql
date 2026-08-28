e762879ed7c74174ba7645e03e84cc0c267813b3
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-08-28 15:15:48 +0100

    Learning Postgres

--- postgres/part3/01-reset-and-seed-relationship.sql
+++ postgres/part3/01-reset-and-seed-relationship.sql
@@ -98,107 +98,107 @@ INSERT INTO tags (name) VALUES
 ('Web Development'),
 ('Tutorial');
 
--- INSERT INTO comments (post_id, body)
--- VALUES
--- (
---     (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
---     'This is a very helpful introduction to PostgreSQL.'
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
---     'I learned a lot from this article.'
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
---     'The explanation of INNER JOIN was really clear.'
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
---     'Can you also explain FULL OUTER JOIN?'
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
---     'Node.js is one of my favorite backend technologies.'
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
---     'Great tutorial for beginners.'
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
---     'The explanation of relationships makes sense.'
--- );
+INSERT INTO comments (post_id, body)
+VALUES
+(
+    (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
+    'This is a very helpful introduction to PostgreSQL.'
+),
+(
+    (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
+    'I learned a lot from this article.'
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
+    'The explanation of INNER JOIN was really clear.'
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
+    'Can you also explain FULL OUTER JOIN?'
+),
+(
+    (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
+    'Node.js is one of my favorite backend technologies.'
+),
+(
+    (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
+    'Great tutorial for beginners.'
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
+    'The explanation of relationships makes sense.'
+);
 
--- INSERT INTO post_tags (post_id, tags_id)
--- VALUES
+INSERT INTO post_tags (post_id, tags_id)
+VALUES
 
--- -- Introduction to PostgreSQL
--- (
---     (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
---     (SELECT id FROM tags WHERE name = 'PostgreSQL')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
---     (SELECT id FROM tags WHERE name = 'Database')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
---     (SELECT id FROM tags WHERE name = 'SQL')
--- ),
+-- Introduction to PostgreSQL
+(
+    (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
+    (SELECT id FROM tags WHERE name = 'PostgreSQL')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
+    (SELECT id FROM tags WHERE name = 'Database')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Introduction to PostgreSQL'),
+    (SELECT id FROM tags WHERE name = 'SQL')
+),
 
--- -- Understanding SQL Joins
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
---     (SELECT id FROM tags WHERE name = 'SQL')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
---     (SELECT id FROM tags WHERE name = 'Database')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
---     (SELECT id FROM tags WHERE name = 'Tutorial')
--- ),
+-- Understanding SQL Joins
+(
+    (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
+    (SELECT id FROM tags WHERE name = 'SQL')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
+    (SELECT id FROM tags WHERE name = 'Database')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding SQL Joins'),
+    (SELECT id FROM tags WHERE name = 'Tutorial')
+),
 
--- -- Getting Started with Node.js
--- (
---     (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
---     (SELECT id FROM tags WHERE name = 'Node.js')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
---     (SELECT id FROM tags WHERE name = 'JavaScript')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
---     (SELECT id FROM tags WHERE name = 'Backend')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
---     (SELECT id FROM tags WHERE name = 'Web Development')
--- ),
+-- Getting Started with Node.js
+(
+    (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
+    (SELECT id FROM tags WHERE name = 'Node.js')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
+    (SELECT id FROM tags WHERE name = 'JavaScript')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
+    (SELECT id FROM tags WHERE name = 'Backend')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Getting Started with Node.js'),
+    (SELECT id FROM tags WHERE name = 'Web Development')
+),
 
--- -- REST API Best Practices
--- (
---     (SELECT id FROM posts WHERE title = 'REST API Best Practices'),
---     (SELECT id FROM tags WHERE name = 'REST API')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'REST API Best Practices'),
---     (SELECT id FROM tags WHERE name = 'Backend')
--- ),
+-- REST API Best Practices
+(
+    (SELECT id FROM posts WHERE title = 'REST API Best Practices'),
+    (SELECT id FROM tags WHERE name = 'REST API')
+),
+(
+    (SELECT id FROM posts WHERE title = 'REST API Best Practices'),
+    (SELECT id FROM tags WHERE name = 'Backend')
+),
 
--- -- Understanding Database Relationships
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
---     (SELECT id FROM tags WHERE name = 'Database')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
---     (SELECT id FROM tags WHERE name = 'SQL')
--- ),
--- (
---     (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
---     (SELECT id FROM tags WHERE name = 'Programming')
--- );
+-- Understanding Database Relationships
+(
+    (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
+    (SELECT id FROM tags WHERE name = 'Database')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
+    (SELECT id FROM tags WHERE name = 'SQL')
+),
+(
+    (SELECT id FROM posts WHERE title = 'Understanding Database Relationships'),
+    (SELECT id FROM tags WHERE name = 'Programming')
+);
 
