008df9b215dc6650148bd2de452f0e1e06b040fb
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-09-07 16:46:55 +0100

    Learning Continues

--- postgres/part3/03-one-to-many-relationship.sql
+++ postgres/part3/03-one-to-many-relationship.sql
@@ -1,8 +1,14 @@
-SELECT users.name AS author_name, posts.title AS post_title
-FROM posts
-INNER JOIN users ON  posts.user_id = users.id;
-
-
+SELECT users.name AS author_name, posts.title AS post_title, posts.status
+FROM users
+INNER JOIN posts ON  posts.user_id = users.id
+ORDER BY users.name, posts.title;
 -- SELECT users.name AS author_name, posts.title AS post_title
 -- FROM users
 -- INNER JOIN posts ON users.id = posts.user_id
+
+SELECT posts.title AS post_title, users.name AS author_name,
+posts.views, posts.status
+FROM posts
+INNER JOIN users ON users.id = posts.user_id
+WHERE posts.status = 'published'
+ORDER BY posts.views;
