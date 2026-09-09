6ad17f0ae5bc22db96883040c6e501612e776e85
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-09-09 16:17:08 +0100

    Learning Continues

---
+++ postgres/part3/08-group-by.sql
@@ -0,0 +1,7 @@
+SELECT u.name AS author_name, 
+COUNT(p.id) AS total_posts,
+SUM(p.views) AS total_views
+FROM users AS u
+LEFT  JOIN posts AS p ON u.id = p.user_id
+GROUP BY u.id, u.name
+HAVING COUNT(p.id) > 2
