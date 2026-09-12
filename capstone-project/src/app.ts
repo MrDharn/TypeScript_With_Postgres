e9b3ea7a029990fdcb259ceedd8e7866a9c5584c
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-09-12 12:41:04 +0100

    Learning Continues

--- capstone-project/src/app.ts
+++ capstone-project/src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
 import { notFound } from './middlewares/notFound'
 import { errorHandler } from './middlewares/errorHandler'
 
 export const runServer = ()=> {
     const app = express()
     app.use(express.json())
+    app.use(urlencoded({extended: true}))
 
     app.use(notFound)
     app.use(errorHandler)
