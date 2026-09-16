fff6f6dc12a42db162ac95f6538be62514f3ec45
Adekanye Oluwatosin Daniel <mydharn@gmail.com>
2026-09-16 15:58:02 +0100

    feat: Learning containarization

--- capstone-project/src/config/env.ts
+++ capstone-project/src/config/env.ts
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+
+export function checkRequiredValue(key: string):string{
+    const value = process.env.key
+    if(!value){
+        throw new Error(`The key ${key} is empty`)
+    }
+
+    return value
+}
 export const env = {
     port: Number(process.env.PORT ?? 3000),
     isProduction: (process.env.NODE_ENV ?? 'development') === 'production',
     isDev: process.env.NODE_ENV ?? 'development',
-    logLevel: process.env.LOG_LEVEL
+    logLevel: process.env.LOG_LEVEL,
+    databaseUrl: checkRequiredValue('DATABASE_URL')
 } as const
